14992fcc3a4f2b20848ab7a9c6e922bf4d3e26d8
hamakihito <hamakihito@gmail.com>
2018-01-27 14:47:50 +0900

    オーナーをつけました

--- contracts/CryptoPuppies.sol
+++ contracts/CryptoPuppies.sol
@@ -2,17 +2,23 @@ pragma solidity ^0.4.18;
 
 contract CryptoPuppies {
   struct Puppy {
+    address owner;
     uint genes;
     string name;
   }
 
   Puppy[] public puppies;
 
-  function getFirstPuppyName() public view returns (string) {
-    return puppies[0].name;
+  function getPuppyName(uint _number) public view returns (string) {
+    return puppies[_number].name;
   }
 
-  function createPuppies(uint _genes, string _name) public {
-    puppies.push(Puppy(_genes, _name));
+  function getPuppyOwner(uint _number) public view returns (address) {
+    return puppies[_number].owner;
   }
+
+  function createPuppies(address _owner, uint _genes, string _name) public {
+    puppies.push(Puppy(_owner, _genes, _name));
+  }
+
 }
